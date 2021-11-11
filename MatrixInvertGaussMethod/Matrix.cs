df853f8ab9899e44b53f5ee6205c33b7aa950d4e
Szymon Janecki <szymon.janecki@codefusion.pl>
2021-11-11 15:38:55 +0100

    feat: add matrix inversion algorithm

--- MatrixInvertGaussMethod/Matrix.cs
+++ MatrixInvertGaussMethod/Matrix.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MatrixInvertGaussMethod
@@ -7,26 +8,119 @@ namespace MatrixInvertGaussMethod
     class Matrix
     {
         private readonly double[,] _matrix;
-        private readonly int _size;
 
-        public Matrix(int size, params double[] elements )
+        public double[,] GetMatrix { get { return this._matrix; } }
+        public int Rows { get { return this._matrix.GetLength(0); } }
+        public int Cols { get { return this._matrix.GetLength(1); } }
+
+
+        //create matrix with certain amoung of rows and columns
+        public Matrix(int rows, int cols, params double[] elements )
         {
-            this._size = size;
-            this._matrix = new double[size, size];
-            for(var i = 0; i < size; i++)
-                for(var j = 0; j < size; j++)
-                    this._matrix[i, j] = elements[i * size + j];
+            this._matrix = new double[rows, cols];
+            for(var i = 0; i < rows; i++)
+                for(var j = 0; j < cols; j++)
+                    this._matrix[i, j] = elements[i * cols + j];
         }
 
-        public void Print()
+        //create matrix from another matrix but with 1 changed row
+        public Matrix(Matrix matrix, double[] row, int rowNumber)
         {
-            for (var i = 0; i < this._size; i++)
+            if (matrix.Cols != row.Length) throw new Exception("Liczba kolumn macierzy musi być równa długości wiersza");
+
+            var newMatrix = matrix.GetMatrix;
+            for(var i = 0; i < matrix.Cols; i++)
             {
-                for (var j = 0; j < this._size; j++)
-                    Console.Write("{0} ", this._matrix[i, j]);
-                Console.WriteLine();
+                newMatrix[rowNumber, i] = row[i];
+            }
+            this._matrix = newMatrix;
+        }
+        
+        //create new identity matrix
+        public Matrix(int size)
+        {
+            this._matrix = new double[size, size];
+
+            for (var i = 0; i < size; i++)
+                for (var j = 0; j < size; j++)
+                    if (i == j) this._matrix[i, j] = 1;
+                    else this._matrix[i, j] = 0;
+
+        }
+
+        public double[] GetRow(int rowNumber)
+        {
+            return Enumerable.Range(0, this.Cols).Select(x => this._matrix[rowNumber, x]).ToArray();
+        }
+
+        public Matrix Concat(Matrix matrix)
+        {
+            if (this.Rows != matrix.Rows) throw new Exception("Concat: Liczba wierzy musi być równa w obu macierzach");
+
+            var resultParams = new double[0];
+
+            for(var i = 0; i < this.Rows; i++)
+            {
+                resultParams = resultParams.Concat(this.GetRow(i)).Concat(matrix.GetRow(i)).ToArray();
             }
 
+            return new Matrix(this.Rows, this.Cols + matrix.Cols, resultParams);
+        }
+
+        public Matrix RowTimesScalar(int rowNumber, double scalar)
+        {
+            var multipliedRow = this.GetRow(rowNumber);
+            for (var i = 0; i < multipliedRow.Length; i++) multipliedRow[i] *= scalar;
+
+            return new Matrix(this, multipliedRow, rowNumber);
+        }
+
+        public Matrix ZeroElem(int rowOneNumber, int rowTwoNumber, double scalar)
+        {
+            var rowOne = this.GetRow(rowOneNumber);
+            var rowTwo = this.GetRow(rowTwoNumber);
+
+            for (var i = 0; i < rowOne.Length; i++) rowOne[i] -= rowTwo[i] * scalar;
+
+            return new Matrix(this, rowOne, rowOneNumber);
+        }
+
+        public Matrix Invert() {
+            Matrix result = this;
+
+            for(var j = 0; j < this.Rows; j++)
+            {
+                result = result.RowTimesScalar(j, 1/result.GetMatrix[j, j]);
+                for(int i = j+1; i < this.Rows; i++)
+                {
+                    result = result.ZeroElem(i, j, result.GetMatrix[i, j]);
+                }
+
+            }
+
+            for(var j = this.Rows - 1; j >= 0; j--)
+            {
+                result = result.RowTimesScalar(j, 1 / result.GetMatrix[j, j]);
+                for (int i = j - 1; i >= 0; i--)
+                {
+                    result = result.ZeroElem(i, j, result.GetMatrix[i, j]);
+                }
+            }
+
+            return result;
+        }
+
+        public Matrix Print()
+        {
+            for (var i = 0; i < this.Rows; i++)
+            {
+                for (var j = 0; j < this.Cols; j++)
+                    Console.Write("{0:0.##}\t", this._matrix[i, j]);
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            return this;
         }
     }
 }
--- MatrixInvertGaussMethod/Program.cs
+++ MatrixInvertGaussMethod/Program.cs
@@ -6,8 +6,22 @@ namespace MatrixInvertGaussMethod
     {
         static void Main(string[] args)
         {
-            Matrix matrix = new Matrix(3, 2, 2, 3, 4, 7, 1, 8, 1, 9);
-            matrix.Print();
+            try
+            {
+                Matrix matrix = new Matrix(3, 3, 2, 2, 3, 4, 7, 1, 8, 1, 9);
+                Matrix matrix2 = new Matrix(3);
+                matrix
+                    .Print()
+                    .Concat(matrix2)
+                    .Print()
+                    .Invert()
+                    .Print();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 }
